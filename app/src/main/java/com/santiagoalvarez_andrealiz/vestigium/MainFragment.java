99f610aeed205fa2a73eff62c980d18b74714f5b
Bhumi Shah <andrealiznm@gmail.com>
2018-05-20 00:11:52 -0500

    Toma la ubicación actual y pone un marker. Escucha cada minuto y entra a la funcion cada vez que hay un cambio de 10 metros

--- app/src/main/java/com/santiagoalvarez_andrealiz/vestigium/MainFragment.java
+++ app/src/main/java/com/santiagoalvarez_andrealiz/vestigium/MainFragment.java
@@ -1,7 +1,13 @@
 package com.santiagoalvarez_andrealiz.vestigium;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -9,6 +15,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,6 +34,12 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+
+import static com.facebook.FacebookSdk.getApplicationContext;
+
 /**
  * Created by andrealiz on 7/05/18.
  */
@@ -34,6 +47,13 @@ import com.google.firebase.auth.FirebaseUser;
 public class MainFragment extends Fragment implements GoogleApiClient.OnConnectionFailedListener, OnMapReadyCallback {
 
     GoogleMap map;
+    LocationManager locationManager;
+
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10 metros
+    //Minimo tiempo para updates en Milisegundos
+    private static final long MIN_TIME_BETWEEN_UPDATES = 1000 * 60 * 1; // 1 minutos
+
+
     public MainFragment(){
 
     }
@@ -49,6 +69,7 @@ public class MainFragment extends Fragment implements GoogleApiClient.OnConnecti
 
 
         return view;*/
+
         View v = inflater.inflate(R.layout.fragment_maps, container, false);
 
 
@@ -73,22 +94,113 @@ public class MainFragment extends Fragment implements GoogleApiClient.OnConnecti
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
-
-        map.setMyLocationEnabled(true);
+        /*map.setMyLocationEnabled(true);
         map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
 
         UiSettings uiSettings = map.getUiSettings();
-        uiSettings.setZoomControlsEnabled(true);
+        uiSettings.setZoomControlsEnabled(true);*/
+
+        if (ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+
+        if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME_BETWEEN_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    double latitude = location.getLatitude();
+                    double longitude = location.getLongitude();
+                    Geocoder geocoder = new Geocoder(getApplicationContext());
+                    try {
+                        List<Address> addressList = geocoder.getFromLocation(latitude, longitude, 1);
+                        String str = addressList.get(0).getLocality() + ",";
+                        str += addressList.get(0).getCountryName();
+                        //Dibujar el marcador
+                        marker(latitude,longitude,str);
+                        //Mostrar cada vez que tome un punto
+                        String date = new Date().toString();
+                        Log.d("map", "take location_NET "+ date);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+                @Override
+                public void onStatusChanged(String s, int i, Bundle bundle) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String s) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String s) {
+
+                }
+            });
+        } else if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME_BETWEEN_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    double latitude = location.getLatitude();
+                    double longitude = location.getLongitude();
+                    Geocoder geocoder = new Geocoder(getApplicationContext());
+                    try {
+                        List<Address> addressList = geocoder.getFromLocation(latitude, longitude, 1);
+                        String str = addressList.get(0).getLocality() + ",";
+                        str += addressList.get(0).getCountryName();
+                        //Dibujar el marcador
+                        marker(latitude,longitude,str);
+                        //Mostrar cada vez que tome un punto
+                        String date = new Date().toString();
+                        Log.d("map", "take location_GPS "+ date);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onStatusChanged(String s, int i, Bundle bundle) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String s) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String s) {
+
+                }
+            });
+        }
 
 
-
-
-        LatLng udea = new LatLng(6.266996,-75.5708923);
+        /*LatLng udea = new LatLng(6.266996,-75.5708923);
         MarkerOptions options = new MarkerOptions();
         options.position(udea).title("UdeA");
         float zoomlevel = 16;
         map.addMarker(options);
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(udea, zoomlevel));
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(udea, zoomlevel));*/
+    }
+
+    public void marker(double lat, double lng, String str){
+        LatLng latLng = new LatLng(lat, lng);
+        map.addMarker(new MarkerOptions().position(latLng).title(str).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+        float zoomlevel = 16;
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoomlevel));
     }
 }
 
