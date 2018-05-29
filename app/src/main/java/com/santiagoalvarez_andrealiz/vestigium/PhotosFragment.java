5bc6b766a3e031e4554ee66f5ac71d12a0abdc7b
Bhumi Shah <andrealiznm@gmail.com>
2018-05-28 22:53:27 -0500

    Funciona RecyclerView TabsActivity

---
+++ app/src/main/java/com/santiagoalvarez_andrealiz/vestigium/PhotosFragment.java
@@ -0,0 +1,24 @@
+package com.santiagoalvarez_andrealiz.vestigium;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by andrealiz on 28/05/18.
+ */
+
+public class PhotosFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_photos, container, false);
+
+        return view;
+
+    }
+}
